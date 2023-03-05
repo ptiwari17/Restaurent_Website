2afd6d315f7399511b23857dad78afe8e4012ac2
Prabhakar Tiwari <pt17.tiwari@gmail.com>
2023-03-05 23:17:01 +0530

    second changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Prabhakar</h1>
+    <h1>Prabhakar tiwari</h1>
     <App />
   </React.StrictMode>
 );
