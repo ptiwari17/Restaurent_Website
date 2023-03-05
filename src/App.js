276b1c98e39d0daa2ca50b236983a60a8d799132
Prabhakar Tiwari <pt17.tiwari@gmail.com>
2023-03-05 23:54:25 +0530

    this at learning phase

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello to worldhy</h1>
+      <h1>hello to world</h1>
     </div>
   );
 }
 
 export default App;
+
